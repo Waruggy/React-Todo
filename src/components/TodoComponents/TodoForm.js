99bec8f25c58e2b1440de67812c9f73d168685d6
Charles <cwang8413@yahoo.com>
2019-06-13 07:53:09 -0700

    Here is part 1.

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Todoform = (props) => {
+    return(
+        <div className='form-div'>            
+            <form onSubmit = {props.AddTodoHandler}>
+                <input className='input' onChange={props.handleTodoChange}
+                    type="text"
+                    name="todo"
+                    value={props.value}
+                    placeholder="...todo"/>
+            </form>    
+                <button className='button add-button' onClick={props.AddTodoHandler}>Add Todo</button>
+                <button className='button remove-button' onClick={props.removeCompleted} >Clear Completed</button>              
+        </div>
+    );
+}
+
+export default Todoform;
